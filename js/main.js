fc73178844acd85ea9fd695de1ad597ae9fa4391
Max Clayton <maxclayton07@gmail.com>
2020-01-09 19:07:52 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+document.getElementById('company').innerHTML = "Joe's Bed and Breakfast";
+
+
+
+document.querySelector('header > h2').innerText = "Decent food!"
+
+let userName = prompt("What is your name?")
+
+//let message = "Hello " + userName + ", welcome to the finest lodging in Utah."
+
+let message = `Hello ${userName}, welcome to the finest lodging int Utah.`
+
+document.querySelector ('#greeting').innerText = message
